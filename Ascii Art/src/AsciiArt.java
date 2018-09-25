1df6390b849c732bb4418777cc55fe15932f448d
Jacob Lurvey <jlurvey19@ssdsnassau.org>
2018-09-25 16:59:16 -0400

    Ascii Art Final

--- Ascii Art/src/AsciiArt.java
+++ Ascii Art/src/AsciiArt.java
@@ -1,13 +1,14 @@
 public class AsciiArt {
 	public static void main(String[] args) {
-		System.out.println("TextLab01, 100 Point Version" + "\n");
-		System.out.println("Measure today's knowledge by yesterday's confusion." + "\n");
-		System.out.println("Bewilderment + Exposure = Obvious" + "\n");
-		System.out.println("# ##### #####    ##### ##### # #    #  ###### #####");
-		System.out.println("# #   # #   #    #   # #   # # ##   #    #    #   ");
-		System.out.println("# #   # #   #    ##### #   # # # #  #    #    #####");
-		System.out.println("# #   # #   #    #     #   # # #  # #    #        #");
-		System.out.println("# ##### #####    #     ##### # #    #    #    #####");
+		System.out.println(" , ; ,   .-'\"\"\"'-.   , ; ,");
+		System.out.println(" \\\\|/ .'          '.  \\|//");
+		System.out.println("  \\-;-/             \\-;-/");
+		System.out.println("  // ;               ; \\\\");
+		System.out.println(" //__; :.         .; ;__\\\\");
+		System.out.println("`-----\\'.'-.....-'.'/-----'");
+		System.out.println("       '.'.-.-,_.'.'");
+		System.out.println("         '(  (..-'");
+		System.out.println("           '-'");		
 	}
 }
 

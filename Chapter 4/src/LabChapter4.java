5d0aaac0e9683317ee27a5bd0616701a2a12f54f
Jacob Lurvey <jlurvey19@ssdsnassau.org>
2018-10-08 13:15:02 -0400

    CH4 Graphics Lab

---
+++ Chapter 4/src/LabChapter4.java
@@ -0,0 +1,44 @@
+
+import java.applet.Applet;
+import java.awt.*;
+import java.lang.Math;;
+
+public class LabChapter4 extends Applet {
+	public void paint(Graphics screen) {
+		
+		//yung square
+		screen.drawRect(25,25,300,300);
+		screen.drawRect(125,125,300,300);
+		
+		//connectors
+		screen.drawLine(25, 25, 125, 125);
+		screen.drawLine(325, 325, 425, 425);
+		screen.drawLine(25, 325, 125, 425);
+		screen.drawLine(325, 25, 425, 125);
+		
+		//yung circle
+		screen.drawOval(75,75,300,300);
+		
+		//horizontal connectors
+		screen.drawOval(75, 115, 300, 225);
+		screen.drawOval(75, 150, 300, 150);
+		screen.drawOval(75, 190,300,75);
+		
+		//vertical connectors
+		screen.drawOval(115, 75, 225, 300);
+		screen.drawOval(150, 75, 150, 300);
+		screen.drawOval(190, 75, 75, 300);
+	
+		//yung triangle
+		screen.drawLine(450,400,1050,400);
+		screen.drawLine(450,400,750,100);
+		screen.drawLine(750,100,1050,400);
+		
+		//cross lines
+		screen.drawLine(750,100,750,400);
+		screen.drawLine(600,250,1050,400);
+		screen.drawLine(900,250,450,400);
+		
+		
+	}
+}
